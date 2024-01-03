9d026390ada54cba73ef17d5509fc32a9ee5be63
Jon Wakeley <jonathan.l.wakeley@gmail.com>
2024-01-02 21:00:22 -0500

    added border option

--- classitag/classitag.py
+++ classitag/classitag.py
@@ -28,7 +28,9 @@ def load_image(image_path: pathlib.Path) -> Image.Image | None:
     return None
 
 
-def draw_overlay(draw: ImageDraw.ImageDraw, width: int, height: int, classification: str) -> None:
+def draw_overlay(
+    draw: ImageDraw.ImageDraw, width: int, height: int, classification: str, outline: bool
+) -> None:
     font = ImageFont.truetype(str(FONT), DEFAULT_FONT_SIZE)
 
     classification_upper = classification.upper()
@@ -52,7 +54,10 @@ def draw_overlay(draw: ImageDraw.ImageDraw, width: int, height: int, classificat
     text_width = text_bbox[2] - text_bbox[0]
     text_height = text_bbox[3] + text_bbox[1]
 
-    text_position_top = ((width - text_width) // 2, (top_overlay_height - text_height) // 2)
+    text_position_top = (
+        (width - text_width) // 2,
+        (top_overlay_height - text_height) // 2 + 1 if outline else 0,
+    )
     text_position_bottom = (
         (width - text_width) // 2,
         height - bottom_overlay_height + (bottom_overlay_height - text_height) // 2,
@@ -61,9 +66,13 @@ def draw_overlay(draw: ImageDraw.ImageDraw, width: int, height: int, classificat
     draw.text(text_position_top, text, font=font, fill=(0, 0, 0))  # type: ignore # noqa: PGH003
     draw.text(text_position_bottom, text, font=font, fill=(0, 0, 0))  # type: ignore # noqa: PGH003
 
+    # Add black outline
+    if outline:
+        draw.rectangle([0, 0, width, height], outline=(0, 0, 0), width=2)  # type: ignore # noqa: PGH003
+
 
 def save_image_with_overlay(
-    img: Image.Image, image_path: pathlib.Path, classification: str
+    img: Image.Image, image_path: pathlib.Path, classification: str, outline: bool
 ) -> None:
     width, height = img.size
 
@@ -74,7 +83,7 @@ def save_image_with_overlay(
     new_img.paste(img, (0, BAR_HEIGHT))
 
     draw = ImageDraw.Draw(new_img)
-    draw_overlay(draw, width, height + 2 * BAR_HEIGHT, classification)
+    draw_overlay(draw, width, height + 2 * BAR_HEIGHT, classification, outline)
 
     new_file_path = image_path.with_stem(f"({classification.upper()})_{image_path.stem}")
     new_img.save(new_file_path.with_suffix(".png"))
@@ -89,38 +98,44 @@ def move_to_original_images(original_path: pathlib.Path, file_path: pathlib.Path
     logging.info(f"Original file moved to: {new_file_path}")
 
 
-def add_overlay_to_directory(directory_path: pathlib.Path, classification: str) -> None:
+def add_overlay_to_directory(
+    directory_path: pathlib.Path, classification: str, outline: bool
+) -> None:
     original_path = directory_path.resolve()
     for file_path in directory_path.glob("*"):
         if file_path.is_file() and file_path.suffix.lower() in {".png", ".jpg", ".jpeg", ".bmp"}:
             img = load_image(file_path)
             if img:
-                save_image_with_overlay(img, file_path, classification)
+                save_image_with_overlay(img, file_path, classification, outline)
                 move_to_original_images(original_path, file_path)
         else:
             logging.warning(f"Skipping unsupported file: {file_path}")
 
 
-def create_command_line_interface(directory_path: pathlib.Path, classification: str) -> None:
+def create_command_line_interface(
+    directory_path: pathlib.Path, classification: str, outline: bool
+) -> None:
     directory_path = pathlib.Path(directory_path)
     if not directory_path.exists() or not directory_path.is_dir():
         logging.error("Invalid directory path. Exiting.")
         return
 
-    add_overlay_to_directory(directory_path, classification)
+    add_overlay_to_directory(directory_path, classification, outline)
 
 
 @click.command()
 @click.argument("directory_path", type=click.Path(exists=True, file_okay=False, dir_okay=True))
 @click.argument("classification", type=click.Choice(["CUI", "SECRET"], case_sensitive=False))
-def main(directory_path: pathlib.Path, classification: str) -> None:
+@click.option("--outline", is_flag=True)
+def main(directory_path: pathlib.Path, classification: str, outline: bool) -> None:
     """
     This project provides a tool for adding classification labels to images within a specified directory. These labels are applied to both the top and bottom of each image, indicating the classification of the content.
 
     directory_path = The path to the directory containing the images.
     classification = The classification type to be applied as an overlay (`CUI` or `SECRET`)
+    outline = Enable an optional border for documentation contrast.
     """
-    create_command_line_interface(directory_path, classification)
+    create_command_line_interface(directory_path, classification, outline)
 
 
 if __name__ == "__main__":
